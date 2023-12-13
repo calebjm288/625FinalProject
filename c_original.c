adf0d5c64da8663e7798f273d8a18daa6889c2b8
Ben Sherwood <bens37@ksu.edu>
2023-12-13 16:02:58 -0600

    Modified c_original.c and added submitserialc.sh

--- c_original.c
+++ c_original.c
@@ -3,7 +3,57 @@
 #include <math.h>
 #include <time.h>
 
-#define rmax 5
+#include <string.h>
+#include <stdint.h>
+
+#include "sys/types.h"
+#include "sys/sysinfo.h"
+
+
+#define rmax 25
+
+
+typedef struct {
+        uint32_t virtualMem;
+        uint32_t physicalMem;
+} processMem_t;
+
+int parseLine(char *line) {
+        // This assumes that a digit will be found and the line ends in " Kb".
+        int i = strlen(line);
+        const char *p = line;
+        while (*p < '0' || *p > '9') p++;
+        line[i - 3] = '\0';
+        i = atoi(p);
+        return i;
+}
+
+void GetProcessMemory(processMem_t* processMem) {
+        FILE *file = fopen("/proc/self/status", "r");
+        char line[128];
+
+        while (fgets(line, 128, file) != NULL) {
+                //printf("%s", line);
+                if (strncmp(line, "VmSize:", 7) == 0) {
+                        processMem->virtualMem = parseLine(line);
+                }
+
+                if (strncmp(line, "VmRSS:", 6) == 0) {
+                        processMem->physicalMem = parseLine(line);
+                }
+        }
+        fclose(file);
+}
+
+processMem_t  checkMemoryMain() {
+        processMem_t myMem;
+
+        GetProcessMemory(&myMem);
+
+        return myMem;
+}
+
+
 
 int main() {
     FILE *file;
@@ -13,8 +63,9 @@ int main() {
     int counts[rmax+1][rmax+1][rmax+1] = {0};
     clock_t start, end;
     double cpu_time_used;
+	processMem_t myMem;
 
-    file = fopen("inputfile300.dat", "r");
+    file = fopen("inputfile100.dat", "r");
     if (file == NULL) {
         perror("Error opening file");
         return 1;
@@ -41,13 +92,14 @@ int main() {
     start = clock();
 
     for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            for (k = 0; k < N; k++) {
-                d12 = sqrt(pow(x[i] - x[j], 2) + pow(y[i] - y[j], 2) + pow(z[i] - z[j], 2));
+        for (j = i+1; j < N; j++) {
+			d12 = sqrt(pow(x[i] - x[j], 2) + pow(y[i] - y[j], 2) + pow(z[i] - z[j], 2));
+			if (d12 > rmax) { continue; }
+            for (k = j+1; k < N; k++) {
                 d23 = sqrt(pow(x[j] - x[k], 2) + pow(y[j] - y[k], 2) + pow(z[j] - z[k], 2));
                 d31 = sqrt(pow(x[k] - x[i], 2) + pow(y[k] - y[i], 2) + pow(z[k] - z[i], 2));
 
-                if (d12 > rmax || d23 > rmax || d31 > rmax) {
+                if (d23 > rmax || d31 > rmax) {
                     continue;
                 } else {
                     counts[(int)d12][(int)d23][(int)d31]++;
@@ -56,7 +108,8 @@ int main() {
             }
         }
     }
-
+	
+	myMem = checkMemoryMain();
     end = clock();
     cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
 
@@ -70,8 +123,9 @@ int main() {
         }
     }
 
-    printf("\nTime taken: %f seconds\n", cpu_time_used);
-    printf("Count: %d\n", count);
+    //printf("\nTime taken: %f seconds\n", cpu_time_used);
+    //printf("Count: %d\n", count);
+	printf("DATA, %u, %u, %f, %d\n", myMem.virtualMem, myMem.physicalMem, cpu_time_used, count);
 
     free(x);
     free(y);
