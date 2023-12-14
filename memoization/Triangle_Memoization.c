948827b39cdee91b5d843b5946b213397878be74
lrnewsom <73595567+lrnewsom@users.noreply.github.com>
2023-12-14 08:27:10 -0600

    Update Triangle_Memoization.c

--- memoization/Triangle_Memoization.c
+++ memoization/Triangle_Memoization.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 #include <time.h>
 
 #define MAX_POINTS 3000
@@ -8,14 +9,39 @@
 
 typedef struct {
     double x, y, z;
+    int index;  // Added index field
 } Point;
 
-// Function to calculate the distance between two points
-double calculate_distance(Point *point1, Point *point2) {
+// Function to calculate the distance between two points with memoization
+double calculate_distance_memoized(Point *point1, Point *point2, double memo[MAX_POINTS][MAX_POINTS]) {
+    int index1 = point1->index;
+    int index2 = point2->index;
+
+    // Check index boundaries
+    if (index1 < 0 || index1 >= MAX_POINTS || index2 < 0 || index2 >= MAX_POINTS) {
+        printf("Error: Invalid indices %d and %d\n", index1, index2);
+        exit(1);
+    }
+
+    if (memo[index1][index2] != 0.0) {
+        // Return memoized result if available
+        printf("Debug: Returning memo[%d][%d]\n", index1, index2);
+        return memo[index1][index2];
+    }
+
     double dx = point1->x - point2->x;
     double dy = point1->y - point2->y;
     double dz = point1->z - point2->z;
-    return sqrt(dx * dx + dy * dy + dz * dz);
+
+    double distance = sqrt(dx * dx + dy * dy + dz * dz);
+
+    printf("Debug: indices %d and %d\n", index1, index2);
+
+    // Memoize the result
+    memo[index1][index2] = distance;
+    memo[index2][index1] = distance;
+
+    return distance;
 }
 
 int main(int argc, char *argv[]) {
@@ -26,7 +52,6 @@ int main(int argc, char *argv[]) {
 
     clock_t start_time = clock();
 
-    // Open the input file
     FILE *file = fopen(argv[1], "r");
     if (file == NULL) {
         perror("Error opening file");
@@ -36,8 +61,8 @@ int main(int argc, char *argv[]) {
     Point points[MAX_POINTS];
     int n = 0;
 
-    // Read data from file
     while (fscanf(file, "%lf %lf %lf", &points[n].x, &points[n].y, &points[n].z) == 3) {
+        points[n].index = n;  // Assign index to the point
         n++;
         if (n >= MAX_POINTS) {
             printf("Too many points. Increase MAX_POINTS if needed.\n");
@@ -48,64 +73,40 @@ int main(int argc, char *argv[]) {
 
     fclose(file);
 
-    // Calculate the maximum number of triangles
-    int max_triangles = n * (n - 1) * (n - 2) / 6;
-    
-    // Initialize histograms dynamically
-    //double *hist12 = malloc(max_triangles * sizeof(double));
-    //double *hist23 = malloc(max_triangles * sizeof(double));
-    //double *hist31 = malloc(max_triangles * sizeof(double));
-
-    //if (hist12 == NULL || hist23 == NULL || hist31 == NULL) {
-    //    perror("Error allocating memory");
-    //    return 1;
-    //}
+    double memo[MAX_POINTS][MAX_POINTS];
+    memset(memo, 0, sizeof(memo));
 
     int count = 0;
 
-    // Generate triangles and update histograms
     for (int i = 0; i < n - 2; i++) {
         for (int j = i + 1; j < n - 1; j++) {
-            double dist12 = calculate_distance(&points[i], &points[j]);
-            if (dist12 >= MAX_DISTANCE){
-		continue;
-	    }
+            double dist12 = calculate_distance_memoized(&points[i], &points[j], memo);
+            if (dist12 >= MAX_DISTANCE) {
+                continue;
+            }
             for (int k = j + 1; k < n; k++) {
-                // Calculate distances between points
-                double dist23 = calculate_distance(&points[j], &points[k]);
-                double dist31 = calculate_distance(&points[k], &points[i]);
+                double dist23 = calculate_distance_memoized(&points[j], &points[k], memo);
+                double dist31 = calculate_distance_memoized(&points[k], &points[i], memo);
 
                 if (dist12 < MAX_DISTANCE && dist23 < MAX_DISTANCE && dist31 < MAX_DISTANCE) {
-                    // Update the histograms
-                   // hist12[count] = dist12;
-                    //hist23[count] = dist23;
-                    //hist31[count] = dist31;
                     count++;
                 }
             }
         }
     }
- 
+
     clock_t end_time = clock();
     double elapsed_time = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
 
-    // Save results to a CSV file
     FILE *csv_file = fopen("results.csv", "a");
     if (csv_file == NULL) {
         perror("Error creating CSV file");
         return 1;
     }
 
-    // Write data
     fprintf(csv_file, "%d, %d, %f\n", n, count, elapsed_time);
 
-    // Close CSV file
     fclose(csv_file);
-    /*
-    // Free dynamically allocated memory
-    free(hist12);
-    free(hist23);
-    free(hist31);
-    */
+
     return 0;
 }
